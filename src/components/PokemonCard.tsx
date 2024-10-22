bf5506a57d671218942b70a3e1147518ac006a0e
fouzy chikouche <fouzy_69@hotmail.fr>
2024-10-22 17:07:33 +0200

    branchtwo

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,25 @@
+function PokemonCard () {
+    let pokemon = pokemonList[1]
+return (
+    <>
+    <figure>
+        {pokemon.imgSrc ? (
+<img src = {pokemon.imgSrc} alt="" />)
+: (<p>???</p>)}
+</figure>
+<figcaption>{pokemon.name}</figcaption>
+</>
+);
+}
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+export default PokemonCard
+
